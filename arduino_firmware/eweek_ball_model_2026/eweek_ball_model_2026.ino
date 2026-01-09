08ffe45f33460ca5ce3efec4569ec951b74dec32
Roozki <zawadzkirowan@gmail.com>
2026-01-09 13:35:52 -0800

    idk

--- arduino_firmware/eweek_ball_model_2026/eweek_ball_model_2026.ino
+++ arduino_firmware/eweek_ball_model_2026/eweek_ball_model_2026.ino
@@ -1,3 +1,13 @@
+//  Eweek balls models 2026 igen
+#include "main.h"
+
+
+// Ultrsonic sensor stuff
+#define SPEED_OF_SOUND_MPS 343 // meters per second
+
+
+
+#define PATRICK_HOUSE_LIMIT_SWITCH_PIN 99
 
 
 
