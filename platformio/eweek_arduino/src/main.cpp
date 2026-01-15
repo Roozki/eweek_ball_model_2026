4e7aa4af6bc7ca3ef6e9e7be4baeb5f916dd30d4
Roozki <zawadzkirowan@gmail.com>
2026-01-14 22:43:57 -0800

    meow meowmewme

--- platformio/eweek_arduino/src/main.cpp
+++ platformio/eweek_arduino/src/main.cpp
@@ -5,10 +5,10 @@
 #define SQUIDWARD_CUP_DETECTION_LOWER_THRESH_MM 50
 
 #define PATRICK_CUP_DETECTION_UPPER_THRESH_MM 60
-#define PATRICK_CUP_DETECTION_LOWER_THRESH_MM 50
+#define PATRICK_CUP_DETECTION_LOWER_THRESH_MM 0//50
 
 #define SPONGEBOB_CUP_DETECTION_UPPER_THRESH_MM 60
-#define SPONGEBOB_CUP_DETECTION_LOWER_THRESH_MM 50
+#define SPONGEBOB_CUP_DETECTION_LOWER_THRESH_MM 0//50
 
 
 Servo spigot_servo;
@@ -22,6 +22,8 @@ spigot_servo.attach(9);
   spigot.close();
   Serial.println("Arduino start");
   sonar_squidward_cup.init();
+  sonar_patrick_cup.init();
+  sonar_spongebob_cup.init();
   
 }
 
@@ -30,11 +32,25 @@ void loop() {
   comms::poll_rx_buffer();
 
   float squidward_cup_distance = sonar_squidward_cup.find_distance_mm();
+  float patrick_cup_distance = sonar_patrick_cup.find_distance_mm();
+  float spongebob_cup_distance = sonar_spongebob_cup.find_distance_mm();
   if(squidward_cup_distance <= SQUIDWARD_CUP_DETECTION_UPPER_THRESH_MM && squidward_cup_distance >= SQUIDWARD_CUP_DETECTION_LOWER_THRESH_MM )
   {
     comms::send(comms::MsgId::squidward_cup, "1");
   } else {
     comms::send(comms::MsgId::squidward_cup, "0");
+  }
+  if(spongebob_cup_distance <= SPONGEBOB_CUP_DETECTION_UPPER_THRESH_MM && squidward_cup_distance >= SQUIDWARD_CUP_DETECTION_LOWER_THRESH_MM )
+  {
+    comms::send(comms::MsgId::spongebob_cup, "1");
+  } else {
+    comms::send(comms::MsgId::spongebob_cup, "0");
+  }
+  if(patrick_cup_distance <= PATRICK_CUP_DETECTION_UPPER_THRESH_MM && squidward_cup_distance >= SQUIDWARD_CUP_DETECTION_LOWER_THRESH_MM )
+  {
+    comms::send(comms::MsgId::patrick_cup, "1");
+  } else {
+    comms::send(comms::MsgId::patrick_cup, "0");
   }
 // spigot_servo.writeMicroseconds(1300);
 
